98de6121ccdec42c5e2f7f93b066dbd09fe4c577
Rodrigo <rodrigomendoza721@gmail.com>
2024-05-27 01:20:41 -0300

    Punto 4 terminado

--- Program.cs
+++ Program.cs
@@ -25,3 +25,55 @@ if (resultado)
     }
     Console.WriteLine($"El invertido del numero ingresado es: {aux}");
 }
+
+
+//Punto 4
+
+Console.WriteLine("Ingrese una cadena de texto: ");
+string cadena1 = Console.ReadLine();
+int longCadena = cadena1.Length;
+Console.WriteLine($"La longitud de la cadena ingresada es: {longCadena}");
+Console.WriteLine("Ingrese una cadena de texto: ");
+string cadena2 = Console.ReadLine();
+string concatenar = cadena1 + cadena2;
+Console.WriteLine($"Las cadenas concatenadas es: {concatenar}");
+string subCadena = concatenar.Substring(0, 4);
+Console.WriteLine($"La subcadenas de las concatenadas es: {subCadena}");
+foreach (char caracter in concatenar)
+{
+    Console.WriteLine(caracter);
+}
+
+Console.WriteLine("Ingrese la palabra a buscar:");
+string palabra = Console.ReadLine();
+int posicion = concatenar.IndexOf(palabra);
+if (posicion != -1){
+    Console.WriteLine($"La palabra '{palabra}' se encuntra en la posicion {posicion} de la cadena '{concatenar}'");
+}
+else
+{
+    Console.WriteLine($"La palabra '{palabra}' no se encuentra en la cadena '{concatenar}'");
+}
+
+string mayusculas = concatenar.ToUpper();
+Console.WriteLine($"Cadena en mayúsculas: {mayusculas}");
+
+string minusculas = concatenar.ToLower();
+Console.WriteLine($"Cadena en minúsculas: {minusculas}");
+
+Console.WriteLine("Por favor, ingrese una cadena de texto con separadores:");
+string cadena11 = Console.ReadLine(); // Ejemplo de entrada: "manzana,banana,naranja,pera"
+
+        
+Console.WriteLine("Ingrese el carácter separador:");
+String separador = Console.ReadLine(); // Ejemplo de entrada: ','
+
+string[] partes = cadena11.Split(separador);
+// Mostrar las partes resultantes por pantalla
+
+Console.WriteLine("Las partes de la cadena son:");
+foreach (string parte in partes)
+{
+    Console.WriteLine(parte);
+}
+
